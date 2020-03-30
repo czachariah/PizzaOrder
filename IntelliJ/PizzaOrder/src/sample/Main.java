1c7a57cbbaf33ed3f710d90335a06003627926cc
czachariah <zachariahchris@yahoo.com>
2020-03-30 03:32:43 -0400

    Got top part of the GUI done for now :)

--- IntelliJ/PizzaOrder/src/sample/Main.java
+++ IntelliJ/PizzaOrder/src/sample/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainStage.fxml"));
         primaryStage.setTitle("Chris and Tony's Pizzeria");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 650, 900));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
