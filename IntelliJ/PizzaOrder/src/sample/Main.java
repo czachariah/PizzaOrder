717c4d2cbf40011ab5a6cbb8503748e659c94191
czachariah <zachariahchris@yahoo.com>
2020-03-30 14:38:28 -0400

    Finished initial layout. Now to figure out backend...

--- IntelliJ/PizzaOrder/src/sample/Main.java
+++ IntelliJ/PizzaOrder/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainStage.fxml"));
         primaryStage.setTitle("Chris and Tony's Pizzeria");
-        primaryStage.setScene(new Scene(root, 650, 900));
+        primaryStage.setScene(new Scene(root, 700, 740));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
--- IntelliJ/PizzaOrder/src/sample/MainStageController.java
+++ IntelliJ/PizzaOrder/src/sample/MainStageController.java
@@ -1,4 +1,45 @@
 package sample;
 
-public class MainStageController {
-}
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MainStageController implements Initializable
+{
+    @FXML
+    public ComboBox<String> PizzaTypeDropDown;
+
+    @FXML
+    public ListView<String> listView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        List<String> toppings = new ArrayList<>();
+        toppings.add("Beef");
+        toppings.add("Cheese");
+        toppings.add("Chicken");
+        toppings.add("Green Pepper");
+        toppings.add("Ham");
+        toppings.add("Mushroom");
+        toppings.add("Onion");
+        toppings.add("Pepperoni");
+        toppings.add("Pineapple");
+        toppings.add("Sausage");
+        listView.setItems(FXCollections.observableArrayList(toppings));
+
+        PizzaTypeDropDown.setValue("Build Your Own");
+        pizzaTypeChange();
+    } // initialize()
+
+    public void pizzaTypeChange()
+    {
+        System.out.println("Selected value : " + PizzaTypeDropDown.getValue());
+    }
+} // MainStageController
